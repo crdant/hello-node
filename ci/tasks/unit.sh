a20894f47fc1c9d561cfd3dae405eb0b966927b6
Chuck D'Antonio <cdantonio@pivotal.io>
2017-07-05 11:47:31 -0700

    Cleaned up arguments

--- ci/tasks/unit.sh
+++ ci/tasks/unit.sh
@@ -8,22 +8,6 @@ while [ $# -gt 0 ]; do
       inputDir=$2
       shift
       ;;
-    -o | --output-dir )
-      outputDir=$2
-      shift
-      ;;
-    -v | --version-file )
-      versionFile=$2
-      shift
-      ;;
-    -a | --artifactId )
-      artifactId=$2
-      shift
-      ;;
-    -p | --packaging )
-      packaging=$2
-      shift
-      ;;
     * )
       echo "Unrecognized option: $1" 1>&2
       exit 1
@@ -40,21 +24,6 @@ error_and_exit() {
 if [ ! -d "$inputDir" ]; then
   error_and_exit "missing input directory: $inputDir"
 fi
-if [ ! -d "$outputDir" ]; then
-  error_and_exit "missing output directory: $outputDir"
-fi
-if [ ! -f "$versionFile" ]; then
-  error_and_exit "missing version file: $versionFile"
-fi
-if [ -z "$artifactId" ]; then
-  error_and_exit "missing artifactId!"
-fi
-if [ -z "$packaging" ]; then
-  error_and_exit "missing packaging!"
-fi
-
-version=`cat $versionFile`
-artifactName="${artifactId}-${version}.${packaging}"
 
 cd $inputDir
 npm run test
